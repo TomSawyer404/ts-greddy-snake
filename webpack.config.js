2e9ec822cbd21e04c900e09ad544ea3db02241a8
banana_windows <tomsawyer404@outlook.com>
2023-04-14 17:54:58 +0800

    build: add plugin and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path'); // NodeJS 模块，帮助我们拼接路径
 
 module.exports = {
@@ -11,12 +12,25 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.tsx?$/, // 正则表达式匹配要编译的文件
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [  // loader 执行顺序由下向上
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
             }
         ]
     }
